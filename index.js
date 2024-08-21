1f03f77e20ed8903f7a98c48688fb47d6f228475
meghna237 <meghucrajesh@gmail.com>
2024-08-21 12:41:21 +0530

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const authRoutes = require('./routes/dbRoutes.js');
+app.use('/api',authRoutes);
+app.use('*', (req, res)=>{
+    res.send("No  Routes")
+})
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port 4000`));
