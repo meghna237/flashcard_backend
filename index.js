c13288edd5ce16c8a428c79b3373ea0c08b71470
meghna237 <meghucrajesh@gmail.com>
2024-08-21 12:42:47 +0530

    first

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const express = require('express');
-const app = express();
+// authRoutes.js
+const router = require('express').Router();
+const flashcardController = require('../controllers/flashcardController');
 
-const cors = require('cors');
-app.use(cors());
+router.post('/login', flashcardController.login);
+router.put('/signup', flashcardController.signup);
+router.get('/subjects',  flashcardController.getSubjects);
+router.post('/subjects', flashcardController.addSubject);
+router.get('/questions', flashcardController.getFlashcardsBySubject);
+router.post('/addquestions', flashcardController.addFlashcard);
+router.delete('/questions/:id', flashcardController.deleteFlashcard);
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-
-const authRoutes = require('./routes/dbRoutes.js');
-app.use('/api',authRoutes);
-app.use('*', (req, res)=>{
-    res.send("No  Routes")
-})
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port 4000`));
+module.exports = router;
